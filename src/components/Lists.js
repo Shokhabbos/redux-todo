b799f5b736ca1ecf6e780a8572c4950b178ca46c
shokhabbos <urinovs999@gmail.com>
2023-05-25 16:29:13 +0500

    not found

--- src/components/Lists.js
+++ src/components/Lists.js
@@ -8,9 +8,13 @@ const Lists = () => {
 
   return (
     <ul className="list-group">
-      {selector.map((todo, i) => (
-        <List key={i} title={todo.text} id={todo.id} />
-      ))}
+      {selector.length > 0 ? (
+        selector.map((todo, i) => (
+          <List key={i} title={todo.text} id={todo.id} />
+        ))
+      ) : (
+        <h3 className="text-center">Todos not found</h3>
+      )}
     </ul>
   );
 };
